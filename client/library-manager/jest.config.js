c4ae7c236fc12915244ad1f5808552fabcd54c66
Summer Young <summeryoung@Summers-Air.localdomain>
2024-06-25 13:50:32 -0700

    Configure css parsing for jest

--- client/library-manager/jest.config.js
+++ client/library-manager/jest.config.js
@@ -3,5 +3,8 @@ export default {
     transform: {
       "^.+\\.tsx?$": "ts-jest",
     },
+    moduleNameMapper: {
+      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   };
